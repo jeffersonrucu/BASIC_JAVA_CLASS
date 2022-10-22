ddd861133bbd439e8ea43adeaf1d4d3497d744ed
Jefferson Oliveira <jefferoidtex7@gmail.com>
2022-10-22 01:33:45 -0300

    feat: cria exemplo simples de class

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import entities.Triangle;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Triangle x, y;
+        x = new Triangle();
+        y = new Triangle();
+
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
+
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
+
+        sc.close();
+    }
+}

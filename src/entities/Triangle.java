e533e15a7b9b354d0482eb6fbbfad8b031cdce5b
Jefferson Oliveira <jefferoidtex7@gmail.com>
2022-10-23 02:04:16 -0300

    doc: adiciona nome do reorema da area

--- src/entities/Triangle.java
+++ src/entities/Triangle.java
@@ -13,6 +13,7 @@ public class Triangle {
     }
 
     public double area() {
+        // Teorema de Herão
         double p = (this.a + this.b + this.c) / 2.0;
         return Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
     }

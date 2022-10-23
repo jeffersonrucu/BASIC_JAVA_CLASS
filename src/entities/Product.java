be250b82c614080a0f6ee4ff3db01dc22251b87c
Jefferson Oliveira <jefferoidtex7@gmail.com>
2022-10-23 01:50:43 -0300

    feat: cria metodo contrutor para class product para deixar parametros de forma obrigatoria

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -2,9 +2,15 @@ package entities;
 
 public class Product {
 
-    public String name;
-    public double price;
-    public int quantity;
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
 
     public double totalValueInStock() {
         return this.price * this.quantity;
